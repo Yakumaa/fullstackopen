0ef409dec45732e42b5585e5488a984308a6a5bd
Yakumaa <maharjanshrish8@gmail.com>
2024-03-05 18:12:33 +0545

    2.6: creating phonebook and displaying the names

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPhonebook = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    const phonebookObject = {
+      name: newName
+    }
+    setPersons(persons.concat(phonebookObject))
+    setNewName('')
+  }
+
+  const handlePhonebookChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPhonebook}>
+        <div>
+          name: <input value={newName} onChange={handlePhonebookChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person, index) => (
+        <p key={index}>{person.name}</p>
+      ))}
+      <div>debug: {newName}</div>
+    </div>
+    
+  )
+}
+
+export default App
