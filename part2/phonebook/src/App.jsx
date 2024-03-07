9ad7e7147dbb42a56738a5cf338aa222cb854f7a
Shrish Maharjan <maharjanshrish8@gmail.com>
2024-03-07 21:55:59 +0545

    update phonebook entries

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', 
-      number: '040-1234567'}
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
 
@@ -43,6 +45,13 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>
+          filter shown with <input/>
+        </div>
+      </form>
+
+      <h2>add a new</h2>
       <form onSubmit={addPhonebook}>
         <div>
           name: <input value={newName} onChange={handlePhonebookNameChange}/>
