60b3db2ffdee5878caa7d215ea04ad07c0e10152
Yakumaa <maharjanshrish8@gmail.com>
2024-03-20 22:43:36 +0545

    2.11: Add phonebook database and fetched using effect hook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = (props) => {
   const { searchName, handleSearchNameChange } = props
@@ -34,7 +35,7 @@ const PersonForm = (props) => {
 
 const Person = (props) => {
   const { person, index } = props
-  console.log(props)
+  // console.log(props)
 
   return (
     <p key={index}>{person.name} {person.number}</p>
@@ -42,15 +43,21 @@ const Person = (props) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchName, setSearchName] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'persons')
 
   const addPhonebook = (event) => {
     event.preventDefault()
